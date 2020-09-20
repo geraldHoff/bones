7301f2a0208df851d07d4305583921ac56375390
gerald <greengorilla3@protonmail.com>
2020-09-19 17:55:28 -0700

    Added Bone Pickaxe item, no texture yet.

--- src/main/java/net/bones/Bones.java
+++ src/main/java/net/bones/Bones.java
@@ -1,10 +1,17 @@
 package net.bones;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class Bones implements ModInitializer {
+
+    public static final Item BONE_PICKAXE = new Item(new Item.Settings().group(ItemGroup.TOOLS));
+
     @Override
     public void onInitialize() {
-
+        Registry.register(Registry.ITEM, new Identifier("bones", "bone_pickaxe"), BONE_PICKAXE);
     }
 }
